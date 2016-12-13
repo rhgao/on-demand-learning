d0314208ec4a29366a40ab340541eafd445d3db9
Ruohan Gao <rhgao@cs.utexas.edu>
2016-12-13 16:00:28 -0600

    training scheme

--- training_schemes/on_demand_learning.sh
+++ training_schemes/on_demand_learning.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# This is the sample script for the on-demand learning training scheme. Please adapt it for your usage based on your own desired validation implementation. The idea is to validate regularly to assess the performance of the snapshot model. The assessment can be based on different factors of your own design, like the average reconstruction loss, average PSNR (currently used in the arXiv paper), etc. The optimal way to get the performance scores for each sub-tasks will be released in the future after publication at a conference.
+# This is the sample script for the on-demand learning training scheme. 
+# Please adapt it for your usage based on your own desired validation implementation. 
+# The idea is to validate regularly to assess the performance of the snapshot model. 
+# The assessment can be based on different factors of your own design, like the average reconstruction loss, average PSNR (currently used in the arXiv paper), etc. 
+# The optimal way to get the performance scores for each sub-tasks will be released in the future after publication at a conference.
 
 # Train for one epoch to get an initial model
 DATA_ROOT=dataset/my_train_set name=task level1=20 level2=20 level3=20 level4=20 level5=20 niter=1 loadSize=96 fineSize=64 display=1 display_iter=50 gpu=1 th train.lua
